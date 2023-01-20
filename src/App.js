f621926347bfc8945e9ea5dff53e19a9aff40b48
Ujwal Vinay <walkerdoodle7@gmail.com>
2023-01-20 10:10:55 +0530

    App.js modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
 import Header from "./components/header";
+import Category from "./components/category-section";
+import Destination from "./components/destination";
+import Booking from "./components/booking";
 import "./styles/style.css"
 function App()
 {
     return (
         <div className="container">
             <Header />
+            < Category />
+            < Destination />
+            <Booking />
         </div>
     )
 }
