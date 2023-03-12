2d1249e8446a68b2a1e673d2ed7cf60dc8dcc17c
Ujwal Vinay <walkerdoodle7@gmail.com>
2023-03-12 13:43:32 +0530

    Category data added

---
+++ src/categoryData.js
@@ -0,0 +1,28 @@
+const CategoryData = [
+    {
+        id:1,
+        img : "../images/weather.png",
+        title : "Calculated Weather ",
+        desc : "Built Wicket longer admire do barton vanity itself do in it."
+    },
+    {
+        id:2,
+        img : "../images/flight.png",
+        title : "Best Flights",
+        desc : "Engrossed listening. Park gate sell they west hard for the."
+    },
+    {
+        id:3,
+        img : "../images/events.png",
+        title : "Local Events",
+        desc : "Barton vanity itself do in it. Preferd to men it engrossed listening. "
+    },
+    {
+        id:2,
+        img : "../images/custom.png",
+        title : "Customization",
+        desc : "We deliver outsourced aviation services for military customers"
+    }
+]
+
+export default CategoryData;
