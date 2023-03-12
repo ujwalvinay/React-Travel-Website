8462ea116ee57c4797d44dfee5ffdcbd00d98434
Ujwal Vinay <walkerdoodle7@gmail.com>
2023-03-12 13:44:01 +0530

    Destination data added

---
+++ src/destinationData.js
@@ -0,0 +1,25 @@
+const DestinationData = [
+    {
+        id:1,
+        img : "../images/rome.png",
+        destination : "Rome, Italty",
+        price : "5,42",
+        duration : "10"
+    },
+    {
+        id:2,
+        img : "../images/london.jpg",
+        destination : "London, UK",
+        price : "4.2",
+        duration : "12"
+    },
+    {
+        id:3,
+        img : "../images/europe.png",
+        destination : "Full Europe",
+        price : "15",
+        duration : "28"
+    }
+]
+
+export default DestinationData;
